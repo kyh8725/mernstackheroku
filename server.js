c0ebf68c8f8e283ee2697befb652a6b88bdedf6c
Daniel Kim <kyh8725@hotmail.com>
2020-11-02 15:58:54 -0800

    initial

---
+++ server.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+
+//Static folder
+app.use("client/public", express.static(path.join(__dirname, "public")));
+
+// routes
+const vehicleRoute = require("./routes/api/vehicleRoute");
+app.use("/vehicles", vehicleRoute);
+// MongoDB
+const mongoose = require("mongoose");
+const MONGO_URL = process.env.MONGODB_URL;
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to DB");
+});
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+app.listen(PORT, () => {
+  console.log(`Sever listening on port ${PORT}.`);
+});
