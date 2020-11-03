e0345d05ee755ecc9f558435ce910e8612516bdb
Daniel Kim <kyh8725@hotmail.com>
2020-11-02 20:47:48 -0800

    gallary

--- client/src/components/PublicPage/Gallary.js
+++ client/src/components/PublicPage/Gallary.js
@@ -3,19 +3,6 @@ import ReactFancyBox from "react-fancybox";
 import "react-fancybox/lib/fancybox.css";
 
 export default function Gallery() {
-  const pic1 =
-    "https://www.hdcarwallpapers.com/walls/mclaren_p1_supercars_4k-HD.jpg";
-  const pic2 =
-    "https://www.wsupercars.com/wallpapers/Porsche/2019-Porsche-911-GT3-RS-001-1080.jpg";
-  const pic3 =
-    "https://cdn.motor1.com/images/mgl/kPWbN/s1/mclaren-senna-victory-grey.jpg";
-  const pic4 =
-    "https://www.topgear.com/sites/default/files/styles/16x9_1858w/public/news-listicle/image/1_70.jpg?itok=J6JLQWWk";
-  const pic5 =
-    "https://robbreport.com/wp-content/uploads/2016/12/2017-lamborghini-aventador-s-011.jpg?w=1024";
-  const pic6 =
-    "https://cdn.motor1.com/images/mgl/vyjQb/s3/ferrari-488-pista-prova-sul-circuito-di-fiorano.jpg";
-
   return (
     <>
       <div className="project__title-wrap">
@@ -27,8 +14,8 @@ export default function Gallery() {
             <div className="gallary-left-top-image">
               <ReactFancyBox
                 className="gallary-image"
-                thumbnail={pic3}
-                image={pic3}
+                thumbnail="/pic3.jpg"
+                image="/pic3.jpg"
               />
             </div>
           </div>
@@ -36,15 +23,15 @@ export default function Gallery() {
             <div className="gallary-left-bottom-left-image">
               <ReactFancyBox
                 className="gallary-image"
-                thumbnail={pic2}
-                image={pic2}
+                thumbnail="/pic2.jpg"
+                image="/pic2.jpg"
               />
             </div>
             <div className="gallary-left-bottom-right-image">
               <ReactFancyBox
                 className="gallary-image"
-                thumbnail={pic4}
-                image={pic4}
+                thumbnail="/pic4.jpg"
+                image="/pic4.jpg"
               />
             </div>
           </div>
@@ -54,15 +41,15 @@ export default function Gallery() {
             <div className="gallary-right-top-left-image">
               <ReactFancyBox
                 className="gallary-image"
-                thumbnail={pic5}
-                image={pic5}
+                thumbnail="/pic5.jpg"
+                image="/pic5.jpg"
               />
             </div>
             <div className="gallary-right-top-right-image">
               <ReactFancyBox
                 className="gallary-image"
-                thumbnail={pic1}
-                image={pic1}
+                thumbnail="/pic1.jpg"
+                image="/pic1.jpg"
               />
             </div>
           </div>
@@ -70,8 +57,8 @@ export default function Gallery() {
             <div className="gallary-right-bottom-image">
               <ReactFancyBox
                 className="gallary-image"
-                thumbnail={pic6}
-                image={pic6}
+                thumbnail="/pic6.jpg"
+                image="/pic6.jpg"
               />
             </div>
           </div>
