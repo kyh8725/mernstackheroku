c25a9a3d701f2a72e30ec64db065d220b84f1a2f
Daniel Kim <kyh8725@hotmail.com>
2020-11-03 00:24:35 -0800

    gallary

--- server.js
+++ server.js
@@ -10,19 +10,15 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
 
-//Static folder
-app.use("client/public", express.static(path.join(__dirname, "public")));
-
 // routes
 const vehicleRoute = require("./routes/api/vehicleRoute");
 app.use("/vehicles", vehicleRoute);
+
 // MongoDB
 const mongoose = require("mongoose");
 const MONGO_URL = process.env.MONGODB_URL;
@@ -35,14 +31,19 @@ mongoose.connection.on("connected", () => {
   console.log("Connected to DB");
 });
 
+//Static public folder
+app.use("client/public", express.static(path.join(__dirname, "public")));
+
 if (process.env.NODE_ENV === "production") {
   // Set static folder
+
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
 app.listen(PORT, () => {
   console.log(`Sever listening on port ${PORT}.`);
 });
