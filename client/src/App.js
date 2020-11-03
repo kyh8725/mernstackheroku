47f5ca143edec1f07eb3b8e9d6edbb61a1173f33
Daniel Kim <kyh8725@hotmail.com>
2020-11-02 19:54:59 -0800

    react mongoose

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>MERNSTACK</p>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import Header from "./components/PublicPage/Header";
+import LoginPage from "./components/LoginPage";
+import PublicPage from "./components/PublicPage";
+import ProtectedPage from "./components/ProtectedPage";
+import Contact from "./components/PublicPage/Contact";
+import Footer from "./components/PublicPage/Footer";
+import Vehicles from "./components/PublicPage/Vehicles";
+import "./styles/index.css";
+import "react-fancybox/lib/fancybox.css";
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Header />
+        <div className="app">
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return <Redirect to="/home" />;
+            }}
+          />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/home" component={PublicPage} />
+          <Route path="/vehicles" component={Vehicles} />
+          <Route path="/contact" component={Contact} />
+          <PrivateRoute path="/protected" component={ProtectedPage} />
+        </div>
+        <Footer />
+      </Router>
+    );
+  }
+}
