89893d062e0a59a03e1ae42ef0aa25c21442e3e8
Daniel Kim <kyh8725@hotmail.com>
2020-11-03 00:52:57 -0800

    gallary css

--- client/src/components/PublicPage/Gallary.js
+++ client/src/components/PublicPage/Gallary.js
@@ -11,30 +11,60 @@ export default function Gallery() {
         <div className="gallary-left">
           <div className="gallary-left-top">
             <div className="gallary-left-top-image">
-              <ModalImage small="/pic3.jpg" large="/pic3.jpg" alt="pic3" />
+              <ModalImage
+                className="small-img"
+                small="/pic3.jpg"
+                large="/pic3.jpg"
+                alt="pic3"
+              />
             </div>
           </div>
           <div className="gallary-left-bottom">
             <div className="gallary-left-bottom-left-image">
-              <ModalImage small="/pic2.jpg" large="/pic2.jpg" alt="pic2" />
+              <ModalImage
+                className="small-img"
+                small="/pic2.jpg"
+                large="/pic2.jpg"
+                alt="pic2"
+              />
             </div>
             <div className="gallary-left-bottom-right-image">
-              <ModalImage small="/pic4.jpg" large="/pic4.jpg" alt="pic4" />
+              <ModalImage
+                className="small-img"
+                small="/pic4.jpg"
+                large="/pic4.jpg"
+                alt="pic4"
+              />
             </div>
           </div>
         </div>
         <div className="gallary-right">
           <div className="gallary-right-top">
             <div className="gallary-right-top-left-image">
-              <ModalImage small="/pic5.jpg" large="/pic5.jpg" alt="pic5" />
+              <ModalImage
+                className="small-img"
+                small="/pic5.jpg"
+                large="/pic5.jpg"
+                alt="pic5"
+              />
             </div>
             <div className="gallary-right-top-right-image">
-              <ModalImage small="/pic1.jpg" large="/pic1.jpg" alt="pic1" />
+              <ModalImage
+                className="small-img"
+                small="/pic1.jpg"
+                large="/pic1.jpg"
+                alt="pic1"
+              />
             </div>
           </div>
           <div className="gallary-right-bottom">
             <div className="gallary-right-bottom-image">
-              <ModalImage small="/pic6.jpg" large="/pic6.jpg" alt="pic6" />
+              <ModalImage
+                className="small-img"
+                small="/pic6.jpg"
+                large="/pic6.jpg"
+                alt="pic6"
+              />
             </div>
           </div>
         </div>
