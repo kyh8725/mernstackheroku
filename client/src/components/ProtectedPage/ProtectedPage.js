d9ae3cbeffd9b79125fddfb62794d989737b5aee
Daniel Kim <kyh8725@hotmail.com>
2020-11-09 23:28:22 -0800

    new user

--- client/src/components/ProtectedPage/ProtectedPage.js
+++ client/src/components/ProtectedPage/ProtectedPage.js
@@ -16,15 +16,17 @@ export default class ProtectedPage extends Component {
   }
 
   createUser = () => {
-    const newUserName = this.props.user.userName || this.props.user.displayName;
+    const newUserName = this.props.user.username || this.props.user.displayName;
     if (!this.state.users.includes(this.props.user.userName)) {
       axios
         .post(`${this.state.API_URL}/users/newUser`, {
           userName: newUserName,
         })
         .then((response) => {
-          console.log("creatUser", response.data);
+          console.log(`${newUserName}, created"`);
         });
+    } else {
+      console.log("user already exists");
     }
   };
 
@@ -39,7 +41,6 @@ export default class ProtectedPage extends Component {
             <strong>This page is under construction </strong>
           </p>
           <p>saved vehicles will be shown here</p>
-          <p>you can check their specs, mileasges, maintenance info and etc.</p>
           <h1>{this.props.user.username || this.props.user.displayName}</h1>
         </section>
       </>
